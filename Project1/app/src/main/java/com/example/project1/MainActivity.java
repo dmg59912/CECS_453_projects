aa847d422276ad1747892828422deedc4e85d1e8
Luke C <lpcolias@gmail.com>
2021-06-06 23:30:27 -0700

    FInalized Signup and application debuggins
    Added the last functionality to the signup page and edited some of the layouts. Debugged so app so it was functional, but it needs use case testing.

--- Project1/app/src/main/java/com/example/project1/MainActivity.java
+++ Project1/app/src/main/java/com/example/project1/MainActivity.java
@@ -26,9 +26,9 @@ import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity  {
 
-    private Button btnlogin;
+    private Button btnlogin, btnsignup;
     private EditText edt_user,edt_pass;
-    public Data check = new Data();
+    public static Data data = new Data();
 
 
     @Override
@@ -39,6 +39,8 @@ public class MainActivity extends AppCompatActivity  {
 
         //matching by ID
         btnlogin = findViewById(R.id.btn_login);
+        btnsignup = findViewById(R.id.btn_signup);
+
         edt_user = findViewById(R.id.edt_user_name);
         edt_pass = findViewById(R.id.edt_password);
 
@@ -51,20 +53,33 @@ public class MainActivity extends AppCompatActivity  {
                 String check_user = edt_user.getText().toString();
                 String check_password =  edt_pass.getText().toString();
 
-                if( check.CheckCredentials(check_user,check_password)== true)
+                if( data.CheckCredentials(check_user,check_password)== true)
                 {
-                    String info = "Welcome " + check_user;
+                    String username = check_user;
                     Intent intent = new Intent(getApplicationContext(),welcomeActivity.class);
-                    intent.putExtra("Info",info);
+                    intent.putExtra("Username",username);
                     startActivity(intent);
                 }
                 else
                 {
-                    Toast.makeText(getApplicationContext(),"Failed to log in " ,Toast.LENGTH_LONG).show();
+                    Toast.makeText(getApplicationContext(),"Invalid username or password" ,Toast.LENGTH_LONG).show();
+                    edt_pass.setText("");
                 }
 
             }
         });
+
+        btnsignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+
+                Intent signup = new Intent(getApplicationContext(), signupActivity.class);
+
+                startActivity(signup);
+
+            }
+        });
+
 
 
     }
