586102db584b38072826250626d52489d22ba4c1
Luke C <lpcolias@gmail.com>
2021-06-15 17:47:53 -0700

    Revert "Added background color to test fragment setup"
    This reverts commit 6c8f3f40ed676c344275c9c0d9ee5fc72ed3a22f.

--- Project2/app/src/main/java/com/example/project2/MainActivity.java
+++ Project2/app/src/main/java/com/example/project2/MainActivity.java
@@ -6,9 +6,6 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    private imageFragment imgFrag;
-    private controlFragment ctrlFrag;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
